2558a2939f6f3432d0f024786c65124542667d55
Boguslaw Skrzypkowiak <bskrzypkowiak@ydp.pl>
2016-02-25 11:58:41 +0100

    packages configure CSSOM, some cleaning

--- config.js
+++ config.js
@@ -15,11 +15,9 @@ System.config({
         "version.json": "./dev/version.json!text",
         "URIjs": "medialize/URI.js",
         "zip-ext": "gildas-lormeau/zip.js/WebContent/zip-ext",
-        "text": "github:systemjs/plugin-text@0.0.4",
-        "underscore": "npm:underscore@1.8.3",
         "readium_shared_js": "github:readium/readium-shared-js@0.22.2/js",
         "readium_js": "readium/readium-js/js",
-          "cryptoJs": "github:brix/crypto-js@3.1.6"
+        "cryptoJs": "github:brix/crypto-js@3.1.6"
       }
     },
     "github:readium/readium-shared-js@0.22.2": {
@@ -30,17 +28,16 @@ System.config({
         "readium_cfi_js": "./build-output/_multiple-bundles/readium-cfi-js",
         "rangy": "timdown/rangy-release",
         "URIjs": "medialize/URI.js",
-        "FontLoader": "bramstein/fontloader",
-        "cssom": "NV/CSSOM"
+        "FontLoader": "bramstein/fontloader"
       }
     }
   },
 
   map: {
-    "NV/CSSOM": "github:NV/CSSOM@0.3.1/build/CSSOM",
     "bramstein/fontloader": "github:bramstein/fontloader@1.2.2",
     "bramstein/jsizes": "github:bramstein/jsizes@0.33.0",
     "brix/crypto-js": "github:brix/crypto-js@3.1.6",
+    "cssom": "npm:cssom@0.3.1",
     "eventemitter3": "github:primus/eventemitter3@1.1.1",
     "gildas-lormeau/zip.js": "github:gildas-lormeau/zip.js@master",
     "hammer": "github:hammerjs/hammer.js@2.0.6",
@@ -48,7 +45,6 @@ System.config({
     "medialize/URI.js": "github:medialize/URI.js@1.17.0",
     "readium/readium-js": "github:readium/readium-js@0.22.2",
     "readium/readium-shared-js": "github:readium/readium-shared-js@0.22.2",
-    "text": "github:systemjs/plugin-text@0.0.4",
     "timdown/rangy-release": "github:timdown/rangy-release@1.3.0",
     "underscore": "npm:underscore@1.8.3"
   }
